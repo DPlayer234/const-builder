0109c745bb1d09b3f5254915180ced39be39ebc5
DPlayer234 <dplayer.235@gmail.com>
2025-08-11 15:52:02 +0200

    add test for raw idents in input

--- tests/no_std.rs
+++ tests/no_std.rs
@@ -46,6 +46,39 @@ impl Drop for Droppable {
     fn drop(&mut self) {}
 }
 
+#[expect(
+    non_camel_case_types,
+    reason = "struct builder types may start with lowercase letter"
+)]
+#[expect(
+    non_upper_case_globals,
+    reason = "generic `r#const` should be uppercase"
+)]
+mod raw {
+    // ensure raw idents compile in every possible position
+    use const_builder::ConstBuilder;
+
+    #[derive(Debug, PartialEq, ConstBuilder)]
+    pub struct r#struct {
+        pub r#fn: r#bool,
+        pub r#type: &'r#static r#str,
+        #[builder(
+            default = None,
+            setter(strip_option),
+            rename = r#pub,
+            rename_generic = r#const,
+        )]
+        pub r#next: r#Option<&'r#static r#struct>,
+    }
+
+    #[allow(dead_code)]
+    #[derive(Debug, PartialEq, ConstBuilder)]
+    #[builder(rename = r#use, rename_fn = r#struct, unchecked(rename = r#let))]
+    struct r#mod {
+        _f: (),
+    }
+}
+
 #[test]
 fn no_std_person() {
     const STEVE: Person<'_> = const {
@@ -101,4 +134,24 @@ fn strip_option() {
 fn transform_sum() {
     let sum = Sum::builder().value(4, 6).build();
     assert_eq!(sum, Sum { value: 10 });
+}
+
+#[test]
+fn raw_idents() {
+    const ROOT: raw::r#struct = raw::r#struct::builder().r#fn(false).r#type("root").build();
+
+    let raw = raw::r#struct::builder()
+        .r#fn(true)
+        .r#type("name")
+        .r#pub(&ROOT)
+        .build();
+
+    assert_eq!(
+        raw,
+        raw::r#struct {
+            r#fn: true,
+            r#type: "name",
+            r#next: Some(&ROOT),
+        }
+    );
 }
